2055f74f7bbce4afe112ce483f039fecc44c68de
Ton Nom <tonemail@example.com>
2025-02-13 15:09:07 +0100

    gestion de l'affichage des reservations

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -8,6 +8,8 @@ export const allRoutes = [
     new Route("/signup", "inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword","changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa","Vos réservations", "/pages/reservations/allResa.html"),
+    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
