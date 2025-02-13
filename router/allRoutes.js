758fed07ae766e1a475207489562e9c63adcf866
Ton Nom <tonemail@example.com>
2025-02-13 08:55:54 +0100

    Ajout modal  Edition/suppression  galerie

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -3,6 +3,11 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "la galerie", "/pages/galerie.html"),
+    new Route("/signin", "connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword","changement de mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
